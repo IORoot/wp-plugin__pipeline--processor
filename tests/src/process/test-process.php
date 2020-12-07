b3fff1eb460e87a48dfd80ef4e3df507948d9088
Andy Pearson <andy@londonparkour.com>
2020-12-07 10:41:18 +0000

    Higher-level process stage test

--- tests/src/process/test-process.php
+++ tests/src/process/test-process.php
@@ -48,14 +48,19 @@ class processTest extends WP_UnitTestCase
         $options = [
             'ue_job_process_id' => [
                 'ue_process_group' => [
-                    'ue_process_enabled' => false,
+                    'ue_process_enabled' => true,
                     'ue_process_id' => "process testing for phpunit"
                 ],
                 'ue_process_collection' => [
                     [
                         'ue_mutation_input_select' => 'post_title',
                         'ue_process_method' => 'record',
-                        'ue_mutation_stack_mutation_stack' => false
+                        'ue_mutation_stack_mutation_stack' => [
+                            [
+                                'acf_fc_layout' => 'uppercase',
+                                'enabled' => true,
+                            ]
+                        ]
                     ]
                 ]
             ]
@@ -90,5 +95,68 @@ class processTest extends WP_UnitTestCase
         $got = count($this->class_instance->collection);
 
         $this->assertEquals($expected, $got);
+    }
+
+    /**
+     * @test
+     *
+     * This implements the set_options method on the process class.
+     *
+     * @testdox Testing the set_options() method.
+     *
+     */
+    public function test_run_method()
+    {
+        /**
+         * Setup - Options
+         */
+        $options = [
+            'ue_job_process_id' => [
+                'ue_process_group' => [
+                    'ue_process_enabled' => true,
+                    'ue_process_id' => "process testing for phpunit"
+                ],
+                'ue_process_collection' => [
+                    [
+                        'ue_mutation_input_select' => 'post_title',
+                        'ue_process_method' => 'record',
+                        'ue_mutation_stack_mutation_stack' => [
+                            [
+                                'acf_fc_layout' => 'uppercase',
+                                'enabled' => true,
+                            ]
+                        ]
+                    ]
+                ]
+            ]
+        ];
+        $this->class_instance->set_options($options);
+
+
+        /**
+         * Setup - Collection
+         */
+        $posts[] = (array) $this->factory->post->create_and_get();
+        $posts[] = (array) $this->factory->post->create_and_get();
+
+        $content = [
+            'ue\\content' => $posts
+        ];
+
+        $this->class_instance->set_collection($content);
+
+
+
+        /**
+         * Expected, Recieved, Asserted
+         */
+        $expected = [
+            [ 'post_title' => "POST TITLE 0000030" ],
+            [ 'post_title' => "POST TITLE 0000031" ],
+        ];
+
+        $received = $this->class_instance->run();
+
+        $this->assertEquals($expected, $received);
     }
 }
